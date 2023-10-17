a140a2a15a16fb7f4858cadf816979b15753d363
garima2004yadav <115277218+garima2004yadav@users.noreply.github.com>
2023-10-17 22:10:23 +0530

    Add files via upload

---
+++ Majorityelement.java
@@ -0,0 +1,11 @@
+import java.util.Hashmap();
+public class Majorityelement {
+    public static void main(String[] args) {
+        int arr[] = {1,2,1,3,1,1};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(key:2,value:1);
+        System.out.println("Value is" + map.get(key:2));
+        System.out.println(map.put(key:4, map.getOrDefault(key:4,defaultValue:0)+1));
+        System.out.println("Map is" +map);
+        }
+    }
